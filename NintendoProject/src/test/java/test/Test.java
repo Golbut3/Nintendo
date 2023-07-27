2f24faced11e54b5e425fba9b7fc8eb0694bd912
Victorgm534 <victorgm534@live.com>
2023-07-27 09:54:48 +0200

    Auto stash before merge of "main" and "origin/main"
    Client Lucas

--- NintendoProject/src/test/java/test/Test.java
+++ NintendoProject/src/test/java/test/Test.java
@@ -7,12 +7,17 @@ import model.Boutique;
 import model.Client;
 
 
+import model.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
 
 		Adresse adresse = new Adresse(15, "rue de Paris", "Saint-Germain-en-Laye");
 		Boutique boutique = new Boutique("Monoprix", adresse);
+		// TODO Auto-generated method stub
+		Client c1 = new Client("Charlet", "Lucas");
+		Client c2 = new Client("Doe", "John");
 
 		// TODO Auto-generated method stub
 		Client c1 = new Client("Charlet", "Lucas");
