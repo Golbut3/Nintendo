ed00a6f6fcc6c1f39c291eb34a6572c1588839cf
lucascharlet <lucas.cds.33@gmail.com>
2023-07-27 10:33:41 +0200

    Modif Client et test

--- NintendoProject/src/main/java/model/Client.java
+++ NintendoProject/src/main/java/model/Client.java
@@ -1,34 +1,58 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client {
 	private String nom;
 	private String prenom;
+	private List<Achat> listeAchat = new ArrayList();
 	
-	public Client(String nom, String prenom) {
+	public Client(String nom, String prenom, List<Achat> listeAchat) {
 		this.nom = nom;
 		this.prenom = prenom;
+		this.listeAchat=listeAchat;
 	}
 
+	
 	public String getNom() {
 		return nom;
 	}
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
 
 	public String getPrenom() {
 		return prenom;
 	}
 
+
+	public List<Achat> getListeAchat() {
+		return listeAchat;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
 
+
+	public void setListeAchat(List<Achat> listeAchat) {
+		this.listeAchat = listeAchat;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", listeAchat=" + listeAchat + "]";
 	}
+
+
+	
+
 	
 	
 
