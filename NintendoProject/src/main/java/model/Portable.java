9bc45c0665b40f7f2ec09e6a77e7551f951902ac
RawadAndraos <rawad_andraos@hotmail.com>
2023-07-27 10:14:28 +0200

    Ajout Classes filles

---
+++ NintendoProject/src/main/java/model/Portable.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Portable extends Console {
+
+	public Portable(String nom, double prix, LocalDate dateDeSortie) {
+		super(nom, prix, dateDeSortie);
+		
+	}
+	
+
+}
---
+++ NintendoProject/src/main/java/model/Salon.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Salon extends Console{
+
+	public Salon(String nom, double prix, LocalDate dateDeSortie) {
+		super(nom, prix, dateDeSortie);
+		
+	}
+
+}
